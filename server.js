d71aa6a60b6fecda78cbdc0efe80154660fab1e4
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-04-21 16:30:16 +0800

    main

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+// Gunakan body-parser untuk menangani data JSON
+app.use(bodyParser.json());
+
+// Koneksi ke MongoDB
+mongoose
+  .connect("mongodb://localhost:27017/trackingDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Error connecting to MongoDB:", err));
+
+// Schema dan model untuk data klik
+const clickSchema = new mongoose.Schema({
+  ip: String,
+  browser: String,
+  location: String,
+  time: Date,
+});
+
+const Click = mongoose.model("Click", clickSchema);
+
+// Endpoint untuk menerima data klik
+app.post("/track", async (req, res) => {
+  const { ip, browser, location } = req.body;
+
+  const newClick = new Click({
+    ip,
+    browser,
+    location,
+    time: new Date(),
+  });
+
+  try {
+    await newClick.save();
+    res.status(200).send("Data klik disimpan");
+  } catch (err) {
+    res.status(500).send("Error menyimpan data klik");
+  }
+});
+
+// Menjalankan server
+app.listen(port, () => {
+  console.log(`Server berjalan di http://localhost:${port}`);
+});
