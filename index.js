0af01e44be870ed296cd4519d9b196317b18a071
ilhamrhmddni <ilhamrhmddni0@gmail.com>
2025-04-21 17:57:35 +0800

    main

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const geoip = require("geoip-lite");
 
 const app = express();
 const PORT = 3000;
-const MONGO_URI = "mongodb://localhost:27017/trackingDB"; // URL MongoDB lokal
+const MONGO_URI =
+  "mongodb+srv://ilhamrhmddni:110402@ilhamrhmddni.fqznp3q.mongodb.net/?retryWrites=true&w=majority&appName=ilhamrhmddni"; // URL MongoDB lokal
 
 // Middleware useragent
 app.use(useragent.express());
